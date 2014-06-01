84474212525ec30f46fb9f9f7bf46f06f23b515c
Paul Wittmann <paul@railslove.com>
2014-06-01 15:15:11 +0200

    Initial commit: can delete contacts - end of "Intercomponent Communication" from the Om Basic Tutorial

---
+++ src/om_tut/core.cljs
@@ -0,0 +1,62 @@
+(ns om-tut.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]
+            [cljs.core.async :refer [put! chan <!]]))
+
+(enable-console-print!)
+
+(def app-state
+  (atom
+    {:contacts
+     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")
+  ))
+
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact))
+)
+
+
+(defn contact-view [contact owner]
+  (reify
+    om/IRenderState
+    (render-state [this {:keys [delete]}]
+      (dom/li nil
+        (dom/span nil (display-name contact))
+        (dom/button #js {:onClick (fn [e] (put! delete @contact))} "Delete")))))
+
+
+(defn contacts-view [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:delete (chan)})
+    om/IWillMount
+    (will-mount [_]
+      (let [delete (om/get-state owner :delete)]
+        (go (loop []
+          (let [contact (<! delete)]
+            (om/transact! app :contacts
+              (fn [xs] (vec (remove #(= contact %) xs))))
+            (recur))))))
+    om/IRenderState
+    (render-state [this {:keys [delete]}]
+      (dom/div nil
+        (dom/h2 nil "Contact list")
+        (apply dom/ul nil
+          (om/build-all contact-view (:contacts app)
+          {:init-state {:delete delete}}))))))
+
+
+(om/root contacts-view app-state
+  {:target (. js/document (getElementById "contacts"))})
