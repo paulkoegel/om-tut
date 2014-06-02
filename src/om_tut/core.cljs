2dd9c7c1634f2a5816f92cc847f7f64de84654bb
Paul Wittmann <paul@railslove.com>
2014-06-02 14:56:49 +0200

    Added input field to add new contacts.

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -2,7 +2,9 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put! chan <!]]))
+            [cljs.core.async :refer [put! chan <!]]
+            [clojure.data :as data]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
@@ -23,8 +25,26 @@
   ))
 
 (defn display-name [{:keys [first last] :as contact}]
-  (str last ", " first (middle-name contact))
-)
+  (str last ", " first (middle-name contact)))
+
+(defn parse-contact [contact-str]
+  (let [[first middle last :as parts] (string/split contact-str #"\s+")
+        [first last middle] (if (nil? last) [first middle] [first last middle])
+        middle (when middle (string/replace middle "." ""))
+        c (if middle (count middle) 0)]
+    (when (>= (count parts) 2)
+      (cond-> {:first first :last last}
+        (== c 1) (assoc :middle-initial middle)
+        (>= c 2) (assoc :middle middle)))))
+
+;; (parse-contact "Gerald J. Sussman")
+
+(defn add-contact [app owner]
+  (let [new-contact (-> (om/get-node owner "new-contact")
+                        .-value
+                        parse-contact)]
+    (when new-contact
+      (om/transact! app :contacts #(conj % new-contact)))))
 
 (defn contact-view [contact owner]
   (reify
@@ -56,7 +76,10 @@
           "Sort by last name")
         (apply dom/ul nil
           (om/build-all contact-view (:contacts app)
-          {:init-state {:delete delete}})))))))
+            {:init-state {:delete delete}}))
+        (dom/div nil
+          (dom/input #js {:type "text" :ref "new-contact"})
+          (dom/button #js {:onClick #(add-contact app owner)} "Add contact")))))))
 
 (om/root contacts-view app-state
   {:target (. js/document (getElementById "contacts"))})
